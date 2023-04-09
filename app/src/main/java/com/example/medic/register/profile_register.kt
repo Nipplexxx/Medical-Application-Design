8e616739a544dda078f5b3edbd3075d29b1a0b67
personinsey@gmail.com <personinsey@gmail.com>
2023-04-10 00:02:40 +0300

    Session 2

--- app/src/main/java/com/example/medic/register/profile_register.kt
+++ app/src/main/java/com/example/medic/register/profile_register.kt
@@ -3,7 +3,11 @@ package com.example.medic.register
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import com.example.medic.MainActivity
 import com.example.medic.R
 
@@ -11,6 +15,23 @@ class profile_register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_register)
+
+        findViewById<Button>(R.id.btninactived2).visibility = View.GONE
+        findViewById<Button>(R.id.btninactivedNone2).visibility = View.VISIBLE
+
+        findViewById<EditText>(R.id.editName).addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                findViewById<Button>(R.id.btninactived2).visibility = View.VISIBLE
+                findViewById<Button>(R.id.btninactivedNone2).visibility = View.GONE
+            }
+
+        })
     }
 
     fun nextActivity(view: View){
